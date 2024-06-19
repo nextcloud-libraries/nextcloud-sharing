3892f2ee4b5630876208124ae9565b85896e7ca3
Ferdinand Thiessen <opensource@fthiessen.de>
2024-06-19 12:00:08 +0200

    feat: Add `ShareType` enum to replace `Type` with more JS native naming
    This also prevents name clashes with other `Type` classes or enums
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+/** @deprecated will be removed with the next version use `ShareType` instead */
 export enum Type {
 	SHARE_TYPE_USER = 0,
 	SHARE_TYPE_GROUP = 1,
@@ -13,4 +19,24 @@ export enum Type {
 	 * @since 26.0.0
 	 */
 	SHARE_TYPE_FEDERATED_GROUP = 14,
+}
+
+export enum ShareType {
+	User = 0,
+	Grup = 1,
+	Link = 3,
+	Email = 4,
+	Remote = 6,
+	/**
+	 * Was called `Circle` before Nextcloud 29
+	 */
+	Team = 7,
+	Guest = 8,
+	RemoteGroup = 9,
+	Room = 10,
+	Deck = 12,
+	/**
+	 * @since 26.0.0
+	 */
+	FederatedGroup = 14,
 }
