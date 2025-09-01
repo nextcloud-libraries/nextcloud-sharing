8352115d6c34ed347ffc57ec37997887014e08f1
Ferdinand Thiessen <opensource@fthiessen.de>
2025-09-02 00:33:59 +0200

    feat: provide API to register inline actions
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/ui/sidebar-action.ts
+++ lib/ui/sidebar-action.ts
@@ -4,24 +4,9 @@
  */
 
 import type { INode } from '@nextcloud/files'
-import type { IShare, ShareType } from '../index.ts'
+import type { IShare } from '../index.ts'
 
-export interface ISidebarActionContext {
-	/**
-	 * The node that should be shared
-	 */
-	node: INode
-
-	/**
-	 * The type of the share (to be created)
-	 */
-	shareType: ShareType
-
-	/**
-	 * The share model if the share already exists
-	 */
-	share?: IShare
-}
+import isSvg from 'is-svg'
 
 export interface ISidebarAction {
 	/**
@@ -35,7 +20,6 @@ export interface ISidebarAction {
 	 * The custom elements identifier must be prefixed with your apps namespace like `oca_myapp-sharing_action`.
 	 * Also the component must implement following properties:
 	 * - `node`: The shared node as `INode` from `@nextcloud/files`.
-	 * - `shareType`: The type of the share as number, see `ShareType` for potential values.
 	 * - `share`: The share model if the share already exists as object of type `IShare` (potentially undefined).
 	 * - `onSave`: A registration method to register a on-save callback which will be called when the share is saved `(callback: () => Promise<void>) => void`
 	 *
@@ -53,10 +37,52 @@ export interface ISidebarAction {
 	/**
 	 * Check if the action is enabled for a specific share type
 	 *
-	 * @param shareType - The share type to check
-	 * @param context - If the share already exists the current share context is passed
+	 * @param share - The share
+	 * @param node - The node that share is about
 	 */
-	enabled(shareType: ShareType, context: ISidebarActionContext): boolean
+	enabled(share: IShare, node: INode): boolean
+}
+
+export interface ISidebarInlineAction {
+	/**
+	 * Unique identifier for the action
+	 */
+	id: string
+
+	/**
+	 * Check if the action is enabled for a specific share type
+	 *
+	 * @param share - The share
+	 * @param node - The node that share is about
+	 */
+	enabled(share: IShare, node: INode): boolean
+
+	/**
+	 * The callback handler when the user selected this action.
+	 *
+	 * @param share - The share
+	 * @param node - The shared node
+	 */
+	exec(share: IShare, node: INode): void | Promise<void>
+
+	/**
+	 * The inline svg icon to use.
+	 */
+	iconSvg: string
+
+	/**
+	 * The localized label of this action
+	 *
+	 * @param share - The current share (might not be created yet)
+	 * @param node - The shared node
+	 */
+	label(share: IShare, node: INode): string
+
+	/**
+	 * Order of the sidebar actions.
+	 * A higher value means the action will be displayed first.
+	 */
+	order: number
 }
 
 /**
@@ -86,8 +112,47 @@ export function registerSidebarAction(action: ISidebarAction): void {
 }
 
 /**
+ * Register a new sidebar action
+ *
+ * @param action - The action to register
+ */
+export function registerSidebarInlineAction(action: ISidebarInlineAction): void {
+	if (!action.id) {
+		throw new Error('Sidebar actions must have an id')
+	}
+	if (typeof action.order !== 'number') {
+		throw new Error('Sidebar actions must have the "order" property')
+	}
+	if (typeof action.iconSvg !== 'string' || !isSvg(action.iconSvg)) {
+		throw new Error('Sidebar actions must have the "iconSvg" property')
+	}
+	if (typeof action.label !== 'function') {
+		throw new Error('Sidebar actions must implement the "label" method')
+	}
+	if (typeof action.exec !== 'function') {
+		throw new Error('Sidebar actions must implement the "exec" method')
+	}
+	if (typeof action.enabled !== 'function') {
+		throw new Error('Sidebar actions must implement the "enabled" method')
+	}
+	window._nc_files_sharing_sidebar_inline_actions ??= new Map<string, ISidebarInlineAction>()
+
+	if (window._nc_files_sharing_sidebar_inline_actions.has(action.id)) {
+		throw new Error(`Sidebar action with id "${action.id}" is already registered`)
+	}
+	window._nc_files_sharing_sidebar_inline_actions.set(action.id, action)
+}
+
+/**
  * Get all registered sidebar actions
  */
 export function getSidebarActions(): ISidebarAction[] {
 	return [...(window._nc_files_sharing_sidebar_actions?.values() ?? [])]
+}
+
+/**
+ * Get all registered sidebar inline actions
+ */
+export function getSidebarInlineActions(): ISidebarInlineAction[] {
+	return [...(window._nc_files_sharing_sidebar_inline_actions?.values() ?? [])]
 }
--- lib/window.d.ts
+++ lib/window.d.ts
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import type { ISidebarAction, ISidebarSection } from './ui/index.ts'
+import type { ISidebarAction, ISidebarInlineAction, ISidebarSection } from './ui/index.ts'
 
 declare global {
 	interface Window {
 		_nc_files_sharing_sidebar_actions?: Map<string, ISidebarAction>
+		_nc_files_sharing_sidebar_inline_actions?: Map<string, ISidebarInlineAction>
 		_nc_files_sharing_sidebar_sections?: Map<string, ISidebarSection>
 	}
 }
